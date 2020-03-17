1b97b983e6319b50bee08e14b3aeafe49bfd510a
Will Hart <clouduniversemc@gmail.com>
2020-03-17 11:47:35 +1100

    Connor working on camera now
    Hidden cube that slowly moves up, if a player gets close to that cube on the Y level, the hidden cube will speed up until its far enough that it goes back to default speed.

---
+++ Assets/Scripts/ReadWriteText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ReadWriteText : MonoBehaviour
+{
+    void CreateText()
+    {
+        // Path of the file
+        string path = Application.dataPath + "/Log.txt";
+
+        // Create File if it doesn't exist
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Testing");
+        }
+
+        // Content of the file
+
+        // Add some text to it
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
